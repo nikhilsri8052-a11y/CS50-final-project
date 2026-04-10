a07b2143e9c5495e8c2d5e9aa8c139c8d437303d
Nikhil Srivastava <nikhilsri8052@gmail.com>
2026-04-10 23:00:38 +0530

    Completed

--- rewrite_app.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-content = '''from planner_app import app
-
-if __name__ == '__main__':
-    app.run(debug=True)
-'''
-Path('app.py').write_text(content, encoding='utf-8')
--- wsgi.py
+++
@@ -1,4 +0,0 @@
-from planner_app import app
-
-if __name__ == '__main__':
-    app.run(debug=True)
