9d5a062bded03515acefcd43bbce90019ecf4ba7
Nikhil Srivastava <nikhilsri8052@gmail.com>
2026-04-10 22:54:04 +0530

    Completed

--- tmp_request_test.py
+++
@@ -1,9 +0,0 @@
-import urllib.request
-
-for path in ['/login', '/register']:
-    url = 'http://127.0.0.1:5000' + path
-    try:
-        with urllib.request.urlopen(url) as resp:
-            print(path, resp.status, len(resp.read()))
-    except Exception as e:
-        print(path, 'ERROR', e)
